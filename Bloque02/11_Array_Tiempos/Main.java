d4c4e71fbec1287e5ec1f1ff15c963b4fafd4a19
Jose Antonio <joseantoniofernandezgago@gmail.com>
2026-06-26 13:17:24 +0000

    Ejercicio terminado

--- Bloque02/11_Array_Tiempos/Main.java
+++ Bloque02/11_Array_Tiempos/Main.java
@@ -1,8 +1,20 @@
 public class Main{
 
     public static void main(String[] args){
-       //Declaramos las variables
-        
+       //Declaramos un array
+       double [] tiempos = {72.4, 72.8, 73.1, 74.0};
+
+       //Imprimimos el tiempo del primer coche
+       System.out.printf("Tiempo del primer coche: %.2f\n", tiempos[0]);
+
+       //Imprimimos el tiempo del tercer coche
+       System.out.printf("Tiempo del primer coche: %.2f\n", tiempos[2]);
+
+       //Modificamos el tiempo del segundo coche
+       tiempos[1] = 72.1;
+       System.out.printf("Vueltamejorada para el coche 2: %.2f\n", tiempos[1]);
+
+
 
     }
 }
