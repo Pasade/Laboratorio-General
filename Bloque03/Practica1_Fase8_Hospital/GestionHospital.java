ede0bcce6b76dc5ecc1af94e45e5725035fa1e92
Jose Antonio Fernández Gago <joseantoniofernandezgago@gmail.com>
2026-07-02 04:33:58 +0000

    Ejercicio terminado

--- Bloque03/Practica1_Fase8_Hospital/GestionHospital.java
+++ Bloque03/Practica1_Fase8_Hospital/GestionHospital.java
@@ -1,8 +1,21 @@
-public class Main{
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestionHospital{
     
     public static void main(String[] args){
 
+        ArrayList<Paciente> listaDeIngresos = new ArrayList<>();
+        Paciente paciente1 = new Paciente("Luis", 34, "Diarrea");
+        Paciente paciente2 = new Paciente("María", 76, "Fractura de cadera");
+        Paciente paciente3 = new Paciente("Aristóteles", 234, "Migraña");
 
+        listaDeIngresos.addAll(List.of(paciente1, paciente2, paciente3));
 
+        System.out.println("Hoy hay un total de " + listaDeIngresos.size() + " pacientes");
+
+        for(Paciente p : listaDeIngresos){
+            p.mostrarFicha();
+        }
     }
 }
